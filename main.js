684a37f45c0741535f2ef7644d3e701b69e764e0
Scorpiondeveshi03 <87188042+Scorpiondeveshi03@users.noreply.github.com>
2022-10-16 17:38:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+song1="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreLeftWrist=0;
+scoreRightWrist=0;
+song2="";
+
+
+function preload(){
+    song1= loadSound("kesariya_song.mp3");
+    song2 = loadSound("fakira song.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0){
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreRightWrist = " + scoreRightWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX =" + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX =" + rightWristX + "rightWristY = " + rightWristY);
+    }
+}
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 600,500);
+
+    fill("#FF0000");
+    stroke("#FF0000")
+
+    circle(leftWristX, leftWristY, 20);
+
+    if(scoreLeftWrist > scoreRightWrist){
+        song1.play();
+        song2.stop();
+
+        song1.setVolume(1);
+        document.getElementById("playbtn").innerHTML = "Kesariya";
+    }
+
+    if(scoreLeftWrist < scoreRightWrist){
+        song2.play();
+        song1.stop();
+
+        song2.setVolume(1);
+        document.getElementById("playbtn").innerHTML = "Fakira";
+    }
+
+
+}
+
+
+
+
